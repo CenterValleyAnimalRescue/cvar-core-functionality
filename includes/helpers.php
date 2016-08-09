e388947b15e03679a77c574f33b5c062bfa1911c
Pea Lutz <pea@glocal.coop>
2016-08-08 21:48:24 -0400

    Added `cvar_get_image_ids_by_terms` helper function

--- includes/helpers.php
+++ includes/helpers.php
@@ -7,3 +7,36 @@
  * @since      0.1.0
  * @license    GPL-2.0+
  */
+
+ /**
+  * Get Image IDs of Images Matching terms
+  *
+  * @since 0.1.0
+  *
+  * @uses get_posts
+  * @uses wp_list_pluck
+  *
+  * @param {string} {array} $terms
+  * @return {array} $image_ids
+  *
+  */
+function cvar_get_image_ids_by_terms( $terms = null ) {
+    $terms = ( is_array( $terms ) ) ? $terms : explode( ',', $terms );
+
+    $args = array(
+        'post_type'     => 'attachment',
+        'tax_query'     => array(
+            array(
+                'taxonomy'  => 'media_category',
+                'field'     => 'term_id',
+                'terms'     => $terms
+            )
+        )
+    );
+
+    $images = get_posts( $args );
+
+    $image_ids = wp_list_pluck( $images, 'ID' );
+
+    return $image_ids;
+}
